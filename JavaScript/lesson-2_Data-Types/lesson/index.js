c59148c0eb24ba2b6e9cc5a988b53a602f6ca33e
TikNersi <tik.nersisyan2000@gmail.com>
2020-10-23 19:54:53 +0300

    AAAAAAAAAA

---
+++ JavaScript/lesson-2_Data-Types/lesson/index.js
@@ -0,0 +1,39 @@
+/* 
+* JS data types 
+* Tvyalneri tipery
+*/
+
+// Dinamic(tuyl tipavorvac lezu)(aysinqn wor popoxakan es stexcum ches nshum inch tipia inch tipi ases kara lini) & Static(hastatun(xist tipavorum))
+
+// Primitive types -------------
+
+// 1 number (..., NaN, infinity -infinity)
+// 2 string
+// 3 boolean(true, false)
+// 4 undefined
+// 5 null 
+
+
+var a = 10;
+var b = 230;
+
+/**
+ * Operators (tiper@)
+ */
+
+// 1. unar (+, -)
+// 2. binar (+, -, *, /, %) (=, +=, -=, *=, /=, %=) (>, <, >=, <=, ==, !=, ===, !==), (&&, ||) 2 popoxakana uzum
+// 3. ternar
+
+
+
+// && ete dzax kam aj kom@ sxala uremn sxaala partadir piti 2 sne jisht ylni
+//(true && false) = false
+//(false && true) = false
+//(false && false) = false
+
+
+//(true || true) = true     KAM
+//(true || false) = true
+//(false || true) = true
+//(false || false) = false
